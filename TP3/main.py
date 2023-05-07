d24fbe045d15fdad541f075e8ac5f03590113d3d
MathieuCt <mathieuelouan@gmail.com>
2023-05-07 22:48:15 +0200

    Début du tp de machine learning, jusqu'à la matrice de corrélation

--- TP3/main.py
+++ TP3/main.py
@@ -3,8 +3,7 @@ import pandas as pd
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
-from scipy.cluster.hierarchy import dendrogram
-
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 
 def ex1():
@@ -28,7 +27,7 @@ def ex1():
     # Use the AgglomerativeClustering for CHA clustering
     agg = AgglomerativeClustering(n_clusters=3, linkage='average', distance_threshold=None)
     # Display the clustering result
-    #axs[1].scatter(X[:, 0], X[:, 1], c=Y)
+    # axs[1].scatter(X[:, 0], X[:, 1], c=Y)
 
     agg = agg.fit_predict(X)
     # Display the clustering result
@@ -36,8 +35,28 @@ def ex1():
     plt.show()
 
 
+def ex2():
+    X, Y = make_blobs(
+        n_samples=30,  # nombre de points
+        n_features=2,  # nombre de caractéristiques
+        centers=3,  # nombre de clusters
+        cluster_std=1.0,  # dispersion des clusters
+        center_box=(-5.0, 5.0),
+        shuffle=False,
+        random_state=12
+        ,  # A préciser pour retrouver le même
+        # ensemble de point à chaque exécution
+    )
+    Z = linkage(X, 'ward')
+    fig = plt.figure(figsize=(25, 10))
+    dn = dendrogram(Z, labels=Y, leaf_rotation=90, leaf_font_size=6)
+    Z = linkage(X, 'single')
+    fig = plt.figure(figsize=(25, 10))
+    dn = dendrogram(Z, labels=Y, leaf_rotation=90, leaf_font_size=6)
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    ex1()
-
-
-
+    # ex1()
+    ex2()
